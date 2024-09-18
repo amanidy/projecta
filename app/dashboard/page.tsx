390dd6ef7bb318a752ddbf3729773dc564e2f714
Amani <amaniarnold08@gmail.com>
2024-09-18 07:50:00 +0300

    Added a new project

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <h1>Dashboard Page</h1>
+            <div>
+                
+                
+                    
+            </div>
+            
+        </div>
+    );
+}
+
