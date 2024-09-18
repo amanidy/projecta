e131d7996264f982378914d722b697990f3e08e6
Amani <amaniarnold08@gmail.com>
2024-09-18 14:49:18 +0300

    Updated the project to use streaming

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import 'tailwindcss/tailwind.css';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
-import DashboardPage from './dashboard/page';
+
 
 
 
@@ -20,7 +20,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         
           
-          /* <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
+         <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -34,7 +34,7 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link> 
           
-          <DashboardPage />
+          
           
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12 gap-4">
